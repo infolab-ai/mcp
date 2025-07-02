220910048a1005413fe2cadb39de5d6772a7af80
Filipp Trigub <filipp.trigub@infolab.ai>
2025-07-02 09:59:22 +0200

    try out http

--- huuh_mcp/instructions.md
+++ huuh_mcp/instructions.md
@@ -2,6 +2,11 @@
 
 docker build . -f smithery.Dockerfile -t huuh_mcp:local && docker run huuh_mcp:local --network
 
-# run locally
+## run locally
 
 uv --directory C:\coding_challanges\Infolab\repos\mcp\ run -m huuh_mcp.server --env-file C:\coding_challanges\Infolab\repos\mcp\huuh_mcp\.env
+
+## comparable servers
+
+https://github.com/ScrapeGraphAI/scrapegraph-mcp/blob/main/Dockerfile
+https://github.com/nickclyde/duckduckgo-mcp-server/blob/main/Dockerfile
--- huuh_mcp/server.py
+++ huuh_mcp/server.py
@@ -267,19 +267,20 @@ def main():
     logger.info("Starting huuh MCP server...")
 
     try:
-        # Run the MCP server with Streamable HTTP transport
-        # sock = socket.socket()
-        # sock.bind(('', 0))
-        # port_number = sock.getsockname()[1]
-        # parser = argparse.ArgumentParser(description="Run MCP Streamable HTTP based server")
-        # parser.add_argument("--port", type=int, default=port_number, help="Localhost port to listen on")
-        # args = parser.parse_args()
+        # logger.info(f"Starting MCP server with STDIO transport")
+        # mcp.run(transport="stdio")
 
-        # logger.info(f"Starting MCP server with Streamable HTTP transport on port {args.port} at path /mcp")
-        mcp.run(transport="stdio")
-        # mcp.run(transport="http",
-        #         port=os.getenv('PORT', args.port),
-        #         path="/mcp")
+        # Run the MCP server with Streamable HTTP transport
+        sock = socket.socket()
+        sock.bind(('', 0))
+        port_number = sock.getsockname()[1]
+        parser = argparse.ArgumentParser(description="Run MCP Streamable HTTP based server")
+        parser.add_argument("--port", type=int, default=port_number, help="Localhost port to listen on")
+        args = parser.parse_args()
+        logger.info(f"Starting MCP server with Streamable HTTP transport on port {args.port} at path /mcp")
+        mcp.run(transport="http",
+                port=os.getenv('PORT', args.port),
+                path="/mcp")
     except KeyboardInterrupt:
         logger.info("Received keyboard interrupt, shutting down...")
     except Exception as e:
